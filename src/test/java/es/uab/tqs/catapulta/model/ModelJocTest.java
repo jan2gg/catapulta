4e6016b58957e08bcd6f6859726cc7e11924027b
jotauve0 <code@netnomad.me>
2025-12-03 00:18:38 +0100

    Codi de test estès per ModelJocTest

--- src/test/java/es/uab/tqs/catapulta/model/ModelJocTest.java
+++ src/test/java/es/uab/tqs/catapulta/model/ModelJocTest.java
@@ -19,19 +19,46 @@ public class ModelJocTest {
     public void testInicialitzaTauler() {
         int[][] tauler = model.getTauler();
         assertEquals(5, tauler.length);
-        assertEquals(5, tauler[0].length);
+        for (int i = 0; i < tauler.length; i++) {
+            assertEquals(5, tauler[i].length);
+        }
+
+        // Verifica que totes les posicions són inicialment buides.
+        for (int[] fila : tauler) {
+            for (int cell : fila) {
+                assertEquals(0, cell); // '0' representa un espai buit
+            }
+        }
     }
 
     @Test
     public void testAddConstruccio() {
+        // Cas 1: Afegir primera construcció
         ModelConstruccio construccio = new ModelConstruccio();
         model.addConstruccio(construccio);
         assertEquals(1, model.getConstruccions().size());
+        
+        // Cas 2: Afegir segona construcció
+        ModelConstruccio altraConstruccio = new ModelConstruccio();
+        model.addConstruccio(altraConstruccio);
+        assertEquals(2, model.getConstruccions().size());
+        
+        // Cas 3: Afegir construcció fora del tauler
+        ModelConstruccio construccioForaDeRang = new ModelConstruccio();
+        assertThrows(IllegalArgumentException.class, () -> model.addConstruccio(construccioForaDeRang));
     }
 
     @Test
-    public void testAtacNoConstruccio() {
+    public void testAtac() {
         boolean resultat = model.atac(2, 2);
         assertFalse(resultat);
+
+        // Valors límit i frontera
+        assertFalse(model.atac(0, 0)); // Cantonada superior esquerra
+        assertFalse(model.atac(0, 4)); // Cantonada superior dreta
+        assertFalse(model.atac(4, 0)); // Cantonada inferior esquerra
+        assertFalse(model.atac(4, 4)); // Cantonada inferior dreta
+        assertFalse(model.atac(-1, -1));    // Fora de límits negatiu
+        assertFalse(model.atac(5, 5)); // Fora de límits positiu
     }
 }
