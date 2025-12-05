54d4f87cc4855c905905afffbd1835cc000ef82c
jotauve0 <code@netnomad.me>
2025-12-05 04:21:23 +0100

    Codi desenvolupat ControladorJoc

--- src/main/java/es/uab/tqs/catapulta/controlador/ControladorJoc.java
+++ src/main/java/es/uab/tqs/catapulta/controlador/ControladorJoc.java
@@ -14,14 +14,39 @@ public class ControladorJoc {
 
     public void iniciaJoc() {
         // Lògica per iniciar el joc, incloent el bucle del joc
+        int[][] tauler = model.getTauler();
+        char[][] taulerChar = converteixTauler(tauler);
+        vista.mostraTauler(taulerChar);
+        vista.mostraMissatges(null);
     }
 
     public boolean jugadaUsuari(int x, int y) {
         // Lògica per processar la jugada de l'usuari
-        return false; // Retorna true si l'atac és un èxit, false en cas contrari
+        int[][] tauler = model.getTauler();
+        
+        if (x < 0 || x >= tauler.length || y < 0 || y >= tauler[0].length) {
+            throw new IndexOutOfBoundsException("Coordenades fora de límits");
+        }
+        
+        return model.atac(x, y);
     }
 
     public void setConstruccions() {
         // Lògica per establir les construccions al tauler
+        int[][] tauler = model.getTauler();
+        
+        if (tauler.length == 0 || tauler[0].length == 0) {
+            throw new IllegalStateException("El tauler no està inicialitzat correctament");
+        }
+    }
+
+    private char[][] converteixTauler(int[][] tauler) {
+        char[][] taulerChar = new char[tauler.length][tauler[0].length];
+        for (int i = 0; i < tauler.length; i++) {
+            for (int j = 0; j < tauler[i].length; j++) {
+                taulerChar[i][j] = tauler[i][j] == 0 ? '~' : 'X';
+            }
+        }
+        return taulerChar;
     }
 }
