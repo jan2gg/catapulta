6fca8baf6543ad857d670df2471db150d4432291
jotauve0 <code@netnomad.me>
2025-12-08 04:08:38 +0100

    Mock object per testejar la Vista

--- src/test/java/es/uab/tqs/catapulta/vista/VistaJocTest.java
+++ src/test/java/es/uab/tqs/catapulta/vista/VistaJocTest.java
@@ -1,5 +1,30 @@
 package es.uab.tqs.catapulta.vista;
 
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+
+import org.junit.jupiter.api.Test;
+
 public class VistaJocTest {
-    // TO DO - Tests per a la classe VistaJoc (Opcional)
+    @Test
+    public void testMockObtenirCoordenades() throws Exception {
+        String entrada = "1\n2\n";
+        var originalIn = System.in;
+        var originalOut = System.out;
+      
+        try {
+            var input = new java.io.ByteArrayInputStream(entrada.getBytes());
+            var out = new java.io.ByteArrayOutputStream();
+        
+            System.setIn(input);
+            System.setOut(new java.io.PrintStream(out));
+
+            VistaJoc vista = new VistaJoc();
+            int[] coords = vista.obtenirCoordenades();
+
+            assertArrayEquals(new int[] {1, 2}, coords);
+        } finally {
+            System.setIn(originalIn);
+            System.setOut(originalOut);
+        }
+    }
 }
